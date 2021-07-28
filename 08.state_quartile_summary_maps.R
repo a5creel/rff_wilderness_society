ca6b9f7f9d03430b148a88eb79a09d60060540c3
a5creel <andie.creel@gmail.com>
2021-07-28 17:30:41 -0600

    total maps

--- 08.state_quartile_summary_maps.R
+++ 08.state_quartile_summary_maps.R
@@ -66,6 +66,46 @@ state_pct_POC <- myWorking_decade %>%
 
 myWorking_decade <- left_join(myWorking_decade, state_pct_POC, by = "state_fips" )
 
+# GETTING QUARTILES ------------------------------------------------------------
+
+# ------- Get quartiles of grants received for each decade ------- #
+myQuants <- myWorking_decade %>%
+  dplyr::group_by(state_fips, merge_year) %>%
+  dplyr::summarise(quants = list(quantile(decade_grants, probs = c(.25, .5, .75)))) %>%
+  unnest_wider(quants) %>%
+  dplyr::rename(decade_grants_25 = `25%`) %>%
+  dplyr::rename(decade_grants_50 = `50%`) %>%
+  dplyr::rename(decade_grants_75 = `75%`)
+#joining back in
+myWorking_decade <- left_join(myWorking_decade, myQuants, by = c("state_fips", "merge_year"))
+rm(myQuants)
+#making a factor variable for the quartiles (by state and year)
+myWorking_decade <- myWorking_decade %>%
+  dplyr::mutate(decade_grants_quants = if_else(decade_grants > decade_grants_75, "4", "")) %>% # fourth quartile 
+  dplyr::mutate(decade_grants_quants = if_else(decade_grants <=decade_grants_75, "3", decade_grants_quants)) %>% # third quartile
+  dplyr::mutate(decade_grants_quants = if_else(decade_grants <= decade_grants_50, "2", decade_grants_quants)) %>% # second quartile
+  dplyr::mutate(decade_grants_quants = if_else(decade_grants <= decade_grants_25, "1", decade_grants_quants)) %>% # first quartile 
+  dplyr::select(-decade_grants_75, -decade_grants_50, - decade_grants_25) %>%
+  dplyr::mutate(decade_grants_quants = as.factor(decade_grants_quants))
+# ------- Get quartiles of amount of grant funding for each decade ------- #
+myQuants <- myWorking_decade %>%
+  dplyr::group_by(state_fips, merge_year) %>%
+  dplyr::summarise(quants = list(quantile(decade_amount, probs = c(.25, .5, .75)))) %>%
+  unnest_wider(quants) %>%
+  dplyr::rename(decade_amount_25 = `25%`) %>%
+  dplyr::rename(decade_amount_50 = `50%`) %>%
+  dplyr::rename(decade_amount_75 = `75%`)
+#joining back in
+myWorking_decade <- left_join(myWorking_decade, myQuants, by = c("state_fips", "merge_year"))
+rm(myQuants)
+#making a factor variable for the quartiles (by state and year)
+myWorking_decade <- myWorking_decade %>%
+  dplyr::mutate(decade_amount_quants = if_else(decade_amount > decade_amount_75, "4", "")) %>% # fourth quartile 
+  dplyr::mutate(decade_amount_quants = if_else(decade_amount <=decade_amount_75, "3", decade_amount_quants)) %>% # third quartile
+  dplyr::mutate(decade_amount_quants = if_else(decade_amount <= decade_amount_50, "2", decade_amount_quants)) %>% # second quartile
+  dplyr::mutate(decade_amount_quants = if_else(decade_amount <= decade_amount_25, "1", decade_amount_quants)) %>% # first quartile 
+  dplyr::select(-decade_amount_75, -decade_amount_50, - decade_amount_25) %>%
+  dplyr::mutate(decade_amount_quants = as.factor(decade_amount_quants))
 # ------- Get quartiles of amount of grant funding PER CAPITA for each decade ------- #
 myQuants <- myWorking_decade %>%
   dplyr::group_by(state_fips, merge_year) %>%
@@ -74,11 +114,9 @@ myQuants <- myWorking_decade %>%
   dplyr::rename(decade_avg_per_cap_amount_25 = `25%`) %>%
   dplyr::rename(decade_avg_per_cap_amount_50 = `50%`) %>%
   dplyr::rename(decade_avg_per_cap_amount_75 = `75%`)
-
 #joining back in
 myWorking_decade <- left_join(myWorking_decade, myQuants, by = c("state_fips", "merge_year"))
 rm(myQuants)
-
 #making a factor variable for the quartiles (by state and year)
 myWorking_decade <- myWorking_decade %>%
   dplyr::mutate(decade_avg_per_cap_amount_quants = if_else(decade_avg_per_cap_amount > decade_avg_per_cap_amount_75, "4", "")) %>% # fourth quartile 
@@ -87,6 +125,25 @@ myWorking_decade <- myWorking_decade %>%
   dplyr::mutate(decade_avg_per_cap_amount_quants = if_else(decade_avg_per_cap_amount <= decade_avg_per_cap_amount_25, "1", decade_avg_per_cap_amount_quants)) %>% # first quartile 
   dplyr::select(-decade_avg_per_cap_amount_75, -decade_avg_per_cap_amount_50, - decade_avg_per_cap_amount_25) %>%
   dplyr::mutate(decade_avg_per_cap_amount_quants = as.factor(decade_avg_per_cap_amount_quants))
+# ------- Get quartiles of number of grants  PER CAPITA for each decade ------- #
+myQuants <- myWorking_decade %>%
+  dplyr::group_by(state_fips, merge_year) %>%
+  dplyr::summarise(quants = list(quantile(decade_avg_per_cap_grants, probs = c(.25, .5, .75), na.rm = TRUE))) %>%
+  unnest_wider(quants) %>%
+  dplyr::rename(decade_avg_per_cap_grants_25 = `25%`) %>%
+  dplyr::rename(decade_avg_per_cap_grants_50 = `50%`) %>%
+  dplyr::rename(decade_avg_per_cap_grants_75 = `75%`)
+#joining back in
+myWorking_decade <- left_join(myWorking_decade, myQuants, by = c("state_fips", "merge_year"))
+rm(myQuants)
+#making a factor variable for the quartiles (by state and year)
+myWorking_decade <- myWorking_decade %>%
+  dplyr::mutate(decade_avg_per_cap_grants_quants = if_else(decade_avg_per_cap_grants > decade_avg_per_cap_grants_75, "4", "")) %>% # fourth quartile 
+  dplyr::mutate(decade_avg_per_cap_grants_quants = if_else(decade_avg_per_cap_grants <=decade_avg_per_cap_grants_75, "3", decade_avg_per_cap_grants_quants)) %>% # third quartile
+  dplyr::mutate(decade_avg_per_cap_grants_quants = if_else(decade_avg_per_cap_grants <= decade_avg_per_cap_grants_50, "2", decade_avg_per_cap_grants_quants)) %>% # second quartile
+  dplyr::mutate(decade_avg_per_cap_grants_quants = if_else(decade_avg_per_cap_grants <= decade_avg_per_cap_grants_25, "1", decade_avg_per_cap_grants_quants)) %>% # first quartile 
+  dplyr::select(-decade_avg_per_cap_grants_75, -decade_avg_per_cap_grants_50, - decade_avg_per_cap_grants_25) %>%
+  dplyr::mutate(decade_avg_per_cap_grants_quants = as.factor(decade_avg_per_cap_grants_quants))
 
 ##################### MAPS: AMOUNT OF GRANT $$ PER CAPITA #####################
 
@@ -124,7 +181,7 @@ state_choropleth(myMap_poc,
   theme(plot.title = element_text(size = 15))
 
 #Saving map
-ggsave(width = 11, height = 8, filename = "Exploratory_Output/PERC_Presentation_Maps/poc.jpeg")
+ggsave(width = 11, height = 8, filename = "Exploratory_Output/PERC_Presentation_Maps/poc_$perCap.jpeg")
 
 
 
@@ -155,13 +212,13 @@ state_choropleth(myMap_pov,
   zoom = continental_us_states) +
   scale_fill_brewer(palette="PuOr")+
   # scale_fill_brewer(palette="Purples")+
-  ggtitle(label = "Difference between top and bottom LWCF spending quartiles in % rural") + 
+  ggtitle(label = "Difference between top and bottom LWCF spending quartiles per capita in % rural") + 
   labs(fill = "Difference in % ")+
   theme(legend.position="bottom") + 
   theme(plot.title = element_text(size = 15))
 
 #Saving map
-ggsave(width = 11, height = 8, filename = "Exploratory_Output/PERC_Presentation_Maps/rural.jpeg")
+ggsave(width = 11, height = 8, filename = "Exploratory_Output/PERC_Presentation_Maps/rural_$perCap.jpeg")
 
 
 # difference in % urban from 1st quart and 4th quart ----------------------------
@@ -191,11 +248,121 @@ state_choropleth(myMap_rural,
   zoom = continental_us_states) +
   # scale_fill_brewer(palette="Purples")+
   scale_fill_brewer(palette="PuOr")+
-  ggtitle(label = "Difference between top and bottom LWCF spending quartiles in % people living below poverty line") + 
+  ggtitle(label = "Difference between top and bottom LWCF spending quartiles per capita in % people living below poverty line") + 
   labs(fill = "Difference in % ")+
   theme(legend.position="bottom")+ 
   theme(plot.title = element_text(size = 15))
 
 #Saving map
-ggsave(width = 11, height = 8, filename = "Exploratory_Output/PERC_Presentation_Maps/poverty.jpeg")
+ggsave(width = 11, height = 8, filename = "Exploratory_Output/PERC_Presentation_Maps/poverty_$perCap.jpeg")
 
+##################### MAPS: TOTAL AMOUNT OF GRANT $$ #####################
+
+
+# difference in % POC from 1st quart and 4th quart  ----------------------------
+
+myMap_poc <- myWorking_decade %>%
+  select(state_fips, fips, merge_year, poc_pct, decade_amount_quants) %>%
+  pivot_wider(names_from = decade_amount_quants, values_from = c(poc_pct)) %>%
+  group_by(state_fips) %>%
+  mutate(poc_pct_4 = mean(`4`, na.rm = TRUE)) %>%
+  mutate(poc_pct_3 = mean(`3`, na.rm = TRUE)) %>%
+  mutate(poc_pct_2 = mean(`2`, na.rm = TRUE)) %>%
+  mutate(poc_pct_1 = mean(`1`, na.rm = TRUE)) %>%
+  select(state_fips, starts_with("poc_pct_")) %>%
+  distinct()
+
+#getting state name for choropleth maps 
+fips_codes <- fips_codes %>%
+  select(state_code, state_name) %>%
+  distinct()
+
+myMap_poc <- left_join(myMap_poc, fips_codes, by = c("state_fips" = "state_code")) %>% 
+  rename(region = state_name) %>%
+  mutate(region = tolower(region)) %>%
+  mutate(value = round(poc_pct_4 - poc_pct_1), 2)
+
+state_choropleth(myMap_poc,
+                 zoom = continental_us_states) +
+  # scale_fill_brewer(palette="YlOrBr", direction = -1)+
+  scale_fill_brewer(palette="PuOr")+
+  ggtitle(label = "Difference between top and bottom LWCF total spending quartiles per capita in % people of color") + 
+  labs(fill = "Difference in % ")+
+  theme(legend.position="bottom") + 
+  theme(plot.title = element_text(size = 15))
+
+#Saving map
+ggsave(width = 11, height = 8, filename = "Exploratory_Output/PERC_Presentation_Maps/poc_$total.jpeg")
+
+
+
+# difference in % poverty from 1st quart and 4th quart  ----------------------------
+
+myMap_pov <- myWorking_decade %>%
+  select(state_fips, fips, merge_year, inc_below_pov_pct, decade_amount_quants) %>%
+  pivot_wider(names_from = decade_amount_quants, values_from = c(inc_below_pov_pct)) %>%
+  group_by(state_fips) %>%
+  mutate(inc_below_pov_pct_4 = mean(`4`, na.rm = TRUE)) %>%
+  mutate(inc_below_pov_pct_3 = mean(`3`, na.rm = TRUE)) %>%
+  mutate(inc_below_pov_pct_2 = mean(`2`, na.rm = TRUE)) %>%
+  mutate(inc_below_pov_pct_1 = mean(`1`, na.rm = TRUE)) %>%
+  select(state_fips, starts_with("inc_below_pov_pct_")) %>%
+  distinct()
+
+#getting state name for choropleth maps 
+fips_codes <- fips_codes %>%
+  select(state_code, state_name) %>%
+  distinct()
+
+myMap_pov <- left_join(myMap_pov, fips_codes, by = c("state_fips" = "state_code")) %>% 
+  rename(region = state_name) %>%
+  mutate(region = tolower(region)) %>%
+  mutate(value = round(inc_below_pov_pct_4 - inc_below_pov_pct_1), 2)
+
+state_choropleth(myMap_pov,
+                 zoom = continental_us_states) +
+  scale_fill_brewer(palette="PuOr")+
+  # scale_fill_brewer(palette="Purples")+
+  ggtitle(label = "Difference between top and bottom LWCF total spending quartiles in % rural") + 
+  labs(fill = "Difference in % ")+
+  theme(legend.position="bottom") + 
+  theme(plot.title = element_text(size = 15))
+
+#Saving map
+ggsave(width = 11, height = 8, filename = "Exploratory_Output/PERC_Presentation_Maps/rural_$total.jpeg")
+
+
+# difference in % urban from 1st quart and 4th quart ----------------------------
+
+myMap_rural <- myWorking_decade %>%
+  select(state_fips, fips, merge_year, rural_pct, decade_amount_quants) %>%
+  pivot_wider(names_from = decade_amount_quants, values_from = c(rural_pct)) %>%
+  group_by(state_fips) %>%
+  mutate(rural_pct_4 = mean(`4`, na.rm = TRUE)) %>%
+  mutate(rural_pct_3 = mean(`3`, na.rm = TRUE)) %>%
+  mutate(rural_pct_2 = mean(`2`, na.rm = TRUE)) %>%
+  mutate(rural_pct_1 = mean(`1`, na.rm = TRUE)) %>%
+  select(state_fips, starts_with("rural_pct_")) %>%
+  distinct()
+
+#getting state name for choropleth maps 
+fips_codes <- fips_codes %>%
+  select(state_code, state_name) %>%
+  distinct()
+
+myMap_rural <- left_join(myMap_rural, fips_codes, by = c("state_fips" = "state_code")) %>% 
+  rename(region = state_name) %>%
+  mutate(region = tolower(region)) %>%
+  mutate(value = round(rural_pct_4 - rural_pct_1), 2)
+
+state_choropleth(myMap_rural,
+                 zoom = continental_us_states) +
+  # scale_fill_brewer(palette="Purples")+
+  scale_fill_brewer(palette="PuOr")+
+  ggtitle(label = "Difference between top and bottom LWCF total spending quartiles in % people living below poverty line") + 
+  labs(fill = "Difference in % ")+
+  theme(legend.position="bottom")+ 
+  theme(plot.title = element_text(size = 15))
+
+#Saving map
+ggsave(width = 11, height = 8, filename = "Exploratory_Output/PERC_Presentation_Maps/poverty_$total.jpeg")
