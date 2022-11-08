72b8794a6b141a1360f5e041313adf5788610750
Andie Creel <a5creel@Andies-MacBook-Air.local>
2022-11-07 22:03:12 -0500

    redid data cleaning to get correct units

--- 09.functions.R
+++ 09.functions.R
@@ -3,17 +3,17 @@
 # Started July 2022
 
 #-------------------------------------------------------------------------------
-# get suits index for quantity and amount for avg med income 
+# get suits index for amount for avg med income 
 #-------------------------------------------------------------------------------
 
-#must be df that has the following variables: avg_med_income_house, amount_per_cap_cum, quantity_per_cap_cum
+#must be df that has the following variables: avg_med_income_house, amount_per_cap_cum
 getSuits <- function(df, x_axis = "Income"){
-  # step one:  proportion of x variables, LWCF funding (amount) & grants (quantity)
+  # step one:  proportion of x variables, LWCF funding (amount) 
   df$income_prop <- df$avg_med_income_house/sum(df$avg_med_income_house, na.rm = TRUE)
   df$poc_prop <- df$avg_poc_pct/sum(df$avg_poc_pct, na.rm = TRUE)
   df$rural_prop <- df$avg_rural_pct/sum(df$avg_rural_pct, na.rm = TRUE)
   df$amount_prop <- df$amount_per_cap_cum/sum(df$amount_per_cap_cum, na.rm = TRUE)
-  df$quantity_prop <- df$quantity_per_cap_cum / sum(df$quantity_per_cap_cum, na.rm = TRUE)
+  # df$quantity_prop <- df$quantity_per_cap_cum / sum(df$quantity_per_cap_cum, na.rm = TRUE)
   df$wealth_prop <- df$ZHVI / sum(df$ZHVI, na.rm = TRUE)
   
   #step two: sort based on prop of variable of interest (ascending in privilege)
@@ -36,7 +36,7 @@ getSuits <- function(df, x_axis = "Income"){
   rural_cumsum <- sum_run(df$rural_prop)
   wealth_cumsum <- sum_run(df$wealth_prop)
   
-  quantity_cumsum <- sum_run(df$quantity_prop)
+  # quantity_cumsum <- sum_run(df$quantity_prop)
   amount_cumsum <- sum_run(df$amount_prop)
   
   # Step four: use trapezoid rule to get area under curve 
@@ -44,26 +44,26 @@ getSuits <- function(df, x_axis = "Income"){
   area_K <- .5 # triangle [(0,0), (0,1), (1,1)]
   
   if (x_axis == "Income"){
-    area_L_quantity <- sum(diff(income_cumsum)*rollmean(quantity_cumsum, 2)) 
+    # area_L_quantity <- sum(diff(income_cumsum)*rollmean(quantity_cumsum, 2)) 
     area_L_amount <- sum(diff(income_cumsum)*rollmean(amount_cumsum, 2)) 
   } else if (x_axis == "POC"){
-    area_L_quantity <- sum(diff(poc_cumsum)*rollmean(quantity_cumsum, 2)) 
+    # area_L_quantity <- sum(diff(poc_cumsum)*rollmean(quantity_cumsum, 2)) 
     area_L_amount <- sum(diff(poc_cumsum)*rollmean(amount_cumsum, 2)) 
   } else if (x_axis == "Rural"){
-    area_L_quantity <- sum(diff(rural_cumsum)*rollmean(quantity_cumsum, 2)) 
+    # area_L_quantity <- sum(diff(rural_cumsum)*rollmean(quantity_cumsum, 2)) 
     area_L_amount <- sum(diff(rural_cumsum)*rollmean(amount_cumsum, 2)) 
   } else if (x_axis == "Wealth"){
-    area_L_quantity <- sum(diff(wealth_cumsum)*rollmean(quantity_cumsum, 2)) 
+    # area_L_quantity <- sum(diff(wealth_cumsum)*rollmean(quantity_cumsum, 2)) 
     area_L_amount <- sum(diff(wealth_cumsum)*rollmean(amount_cumsum, 2))     
   }
   
   # Step five: calculate suits index 
   suits_amount <- ( area_L_amount - area_K)/area_K
-  suits_quantity <- (area_L_quantity - area_K)/area_K
+  # suits_quantity <- (area_L_quantity - area_K)/area_K
   
- return(list(Suits_Amount = suits_amount, Suits_Quantity = suits_quantity, 
+ return(list(Suits_Amount = suits_amount, 
               Graph = list(Amount_cs = amount_cumsum, 
-                           Quantity_cs = quantity_cumsum, 
+                           # Quantity_cs = quantity_cumsum, 
                            Income_cs = income_cumsum,
                            Poc_cs = poc_cumsum,
                            Rural_cs = rural_cumsum,
@@ -83,7 +83,7 @@ printSuits <- function(myR, x_axis = "Income"){
   
   #suits index
   amount_title = paste0("Amount of Funding. Suits Index: ", round(myR$Suits_Amount, 3))
-  quantity_title = paste0("Quantity of Grant. Suits Index: ", round(myR$Suits_Quantity, 3))
+  # quantity_title = paste0("Quantity of Grant. Suits Index: ", round(myR$Suits_Quantity, 3))
   
   if(x_axis == "Income"){
     x_var <- myR$Graph$Income_cs
@@ -110,16 +110,16 @@ printSuits <- function(myR, x_axis = "Income"){
     theme_bw()+ 
     geom_abline(intercept = 0, slope = 1) # neutral line 
   
-  # quantity graph
-  qGraph <- ggplot(as.data.frame(myR$Graph), aes(x=x_var, y=Quantity_cs)) + 
-    geom_point(size = .5, colour = 'darkgreen') +
-    ggtitle(quantity_title, subtitle = "Each point is  a county") +
-    xlab(x_label) + 
-    ylab("Cumulative proportion of grants (quantity per cap)") +
-    theme_bw() + 
-    geom_abline(intercept = 0, slope = 1) # neutral line
+  # # quantity graph
+  # qGraph <- ggplot(as.data.frame(myR$Graph), aes(x=x_var, y=Quantity_cs)) + 
+  #   geom_point(size = .5, colour = 'darkgreen') +
+  #   ggtitle(quantity_title, subtitle = "Each point is  a county") +
+  #   xlab(x_label) + 
+  #   ylab("Cumulative proportion of grants (quantity per cap)") +
+  #   theme_bw() + 
+  #   geom_abline(intercept = 0, slope = 1) # neutral line
   
-  return(list(Amout_Graph = aGraph,  Quantity_Graph = qGraph))
+  return(list(Amout_Graph = aGraph))
 
 }
 
