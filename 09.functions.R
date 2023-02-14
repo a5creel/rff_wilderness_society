8a95a88374fe1fd7eaa2759593ce226ea075e6f4
Andie Creel <andie.creel@gmail.com>
2023-02-14 11:07:25 -0500

    Average investment per cap and rearranging

--- 09.functions.R
+++ 09.functions.R
@@ -6,15 +6,13 @@
 # get suits index for amount for avg med income 
 #-------------------------------------------------------------------------------
 
-#must be df that has the following variables: avg_med_income_house, amount_per_cap_tot
+#must be df that has the following variables: avg_med_income_house, amount_per_cap_avg
 getSuits <- function(df, x_axis = "Income"){
   # step one:  proportion of x variables, LWCF funding (amount) 
   df$income_prop <- df$avg_med_income_house/sum(df$avg_med_income_house, na.rm = TRUE)
   df$poc_prop <- df$avg_poc_pct/sum(df$avg_poc_pct, na.rm = TRUE)
   df$rural_prop <- df$avg_rural_pct/sum(df$avg_rural_pct, na.rm = TRUE)
-  df$amount_prop <- df$amount_per_cap_tot/sum(df$amount_per_cap_tot, na.rm = TRUE)
-  # df$quantity_prop <- df$quantity_per_cap_tot / sum(df$quantity_per_cap_tot, na.rm = TRUE)
-  df$wealth_prop <- df$ZHVI / sum(df$ZHVI, na.rm = TRUE)
+  df$amount_prop <- df$amount_per_cap_avg/sum(df$amount_per_cap_avg, na.rm = TRUE)
   
   #step two: sort based on prop of variable of interest (ascending in privilege)
   if (x_axis == "Income"){
@@ -23,8 +21,6 @@ getSuits <- function(df, x_axis = "Income"){
     df <- arrange(df, -poc_prop) #notice that this is descending % poc, therefore it runs most poc to whitest counties. 
   } else if (x_axis == "Rural"){
     df <- arrange(df, -rural_prop) #most rural to most urban 
-  } else if (x_axis == "Wealth"){
-    df <- arrange(df, wealth_prop) 
   } else {
     stop("You have not specified a viable variable of interest. Options are: Income, POC.")
   }
@@ -34,8 +30,7 @@ getSuits <- function(df, x_axis = "Income"){
   income_totsum <- sum_run(df$income_prop)
   poc_totsum <- sum_run(df$poc_prop)
   rural_totsum <- sum_run(df$rural_prop)
-  wealth_totsum <- sum_run(df$wealth_prop)
-  
+
   # quantity_totsum <- sum_run(df$quantity_prop)
   amount_totsum <- sum_run(df$amount_prop)
   
@@ -44,30 +39,21 @@ getSuits <- function(df, x_axis = "Income"){
   area_K <- .5 # triangle [(0,0), (0,1), (1,1)]
   
   if (x_axis == "Income"){
-    # area_L_quantity <- sum(diff(income_totsum)*rollmean(quantity_totsum, 2)) 
     area_L_amount <- sum(diff(income_totsum)*rollmean(amount_totsum, 2)) 
   } else if (x_axis == "POC"){
-    # area_L_quantity <- sum(diff(poc_totsum)*rollmean(quantity_totsum, 2)) 
     area_L_amount <- sum(diff(poc_totsum)*rollmean(amount_totsum, 2)) 
   } else if (x_axis == "Rural"){
-    # area_L_quantity <- sum(diff(rural_totsum)*rollmean(quantity_totsum, 2)) 
     area_L_amount <- sum(diff(rural_totsum)*rollmean(amount_totsum, 2)) 
-  } else if (x_axis == "Wealth"){
-    # area_L_quantity <- sum(diff(wealth_totsum)*rollmean(quantity_totsum, 2)) 
-    area_L_amount <- sum(diff(wealth_totsum)*rollmean(amount_totsum, 2))     
-  }
+  } 
   
   # Step five: calculate suits index 
   suits_amount <- ( area_L_amount - area_K)/area_K
-  # suits_quantity <- (area_L_quantity - area_K)/area_K
-  
+
  return(list(Suits_Amount = suits_amount, 
               Graph = list(Amount_cs = amount_totsum, 
-                           # Quantity_cs = quantity_totsum, 
                            Income_cs = income_totsum,
                            Poc_cs = poc_totsum,
-                           Rural_cs = rural_totsum,
-                           Wealth_cs = wealth_totsum),
+                           Rural_cs = rural_totsum),
               Variable = x_axis))
 }
 
@@ -83,8 +69,7 @@ printSuits <- function(myR, x_axis = "Income", caption = ""){
   
   #suits index
   amount_caption = paste0("Figue YY: The suits index ", round(myR$Suits_Amount, 3), ". ", caption)
-  # quantity_title = paste0("Quantity of Grant. Suits Index: ", round(myR$Suits_Quantity, 3))
-  
+
   if(x_axis == "Income"){
     x_var <- myR$Graph$Income_cs
     x_label <- "Avg. Median Income: poorest -> richest counties "
@@ -94,9 +79,6 @@ printSuits <- function(myR, x_axis = "Income", caption = ""){
   } else if (x_axis == "Rural"){
     x_var <- myR$Graph$Rural_cs
     x_label <- "Avg. Rurality: most rural -> most urban"
-  } else if (x_axis == "Wealth"){  
-    x_var <- myR$Graph$Wealth_cs
-    x_label <- "Cheapest typical home -> Most Expensive"    
   } else{
     stop("You have not specified a viable variable of interest. Options are: Income, POC.")
   }
@@ -104,7 +86,6 @@ printSuits <- function(myR, x_axis = "Income", caption = ""){
   # amount graph
   aGraph <- ggplot(as.data.frame(myR$Graph), aes(x=x_var, y=Amount_cs)) + 
     geom_point(size = .5, colour = 'steelblue') +
-    # ggtitle(amount_title, subtitle = "Each point is  a county") +
     xlab(x_label) + 
     ylab("Accumulated investment per capita") +
     theme_bw()+ 
@@ -113,15 +94,6 @@ printSuits <- function(myR, x_axis = "Income", caption = ""){
     theme(plot.caption.position = "plot",
           plot.caption = element_text(hjust = 0, size = 12),
           text=element_text(family="serif"))
-  
-  # # quantity graph
-  # qGraph <- ggplot(as.data.frame(myR$Graph), aes(x=x_var, y=Quantity_cs)) + 
-  #   geom_point(size = .5, colour = 'darkgreen') +
-  #   ggtitle(quantity_title, subtitle = "Each point is  a county") +
-  #   xlab(x_label) + 
-  #   ylab("Cumulative proportion of grants (quantity per cap)") +
-  #   theme_bw() + 
-  #   geom_abline(intercept = 0, slope = 1) # neutral line
   
   return(list(Amout_Graph = aGraph))
 
